694871f4cd26b5bfadbaf35ebd2d045642b7798f
Christoph Kronberger <christoph.kronberger@ebcont.com>
2022-12-20 09:07:45 +0100

    Refactoring

--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -3,11 +3,12 @@ package parrot;
 public abstract class Parrot {
     public static final double LOAD_FACTOR = 9.0;
     public static final double BASE_SPEED = 12.0;
+    public static final double MAX_SPEED = 24.0;
     protected final int numberOfCoconuts;
     protected final double voltage;
     protected final boolean isNailed;
 
-    public Parrot(int numberOfCoconuts, double voltage, boolean isNailed) {
+    protected Parrot(int numberOfCoconuts, double voltage, boolean isNailed) {
         this.numberOfCoconuts = numberOfCoconuts;
         this.voltage = voltage;
         this.isNailed = isNailed;
@@ -16,7 +17,6 @@ public abstract class Parrot {
     public abstract double getSpeed();
 
     public double getBaseSpeed(double voltage) {
-        double appleSauce = 24.0;
-        return Math.min(appleSauce, voltage * BASE_SPEED);
+        return Math.min(MAX_SPEED, voltage * BASE_SPEED);
     }
 }
