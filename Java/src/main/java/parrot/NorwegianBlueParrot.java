c1d0c969448b492d0ad3245b5730790acadacdea
Christoph Kronberger <christoph.kronberger@ebcont.com>
2022-12-20 09:20:22 +0100

    Refactoring

--- Java/src/main/java/parrot/NorwegianBlueParrot.java
+++ Java/src/main/java/parrot/NorwegianBlueParrot.java
@@ -2,12 +2,14 @@ package parrot;
 
 public class NorwegianBlueParrot extends Parrot{
 
+    public static final double MAX_SPEED = 24.0;
+
     public NorwegianBlueParrot(ParrotBuilder parrotBuilder) {
         super(parrotBuilder);
     }
 
     @Override
     public double getSpeed() {
-        return (isNailed) ? 0 : getBaseSpeed(voltage);
+        return isNailed ? 0 : Math.min(MAX_SPEED, voltage * BASE_SPEED);
     }
 }
--- Java/src/main/java/parrot/Parrot.java
+++ Java/src/main/java/parrot/Parrot.java
@@ -2,7 +2,6 @@ package parrot;
 
 public abstract class Parrot {
     public static final double BASE_SPEED = 12.0;
-    public static final double MAX_SPEED = 24.0;
     protected final int numberOfCoconuts;
     protected final double voltage;
     protected final boolean isNailed;
@@ -15,7 +14,4 @@ public abstract class Parrot {
 
     public abstract double getSpeed();
 
-    public double getBaseSpeed(double voltage) {
-        return Math.min(MAX_SPEED, voltage * BASE_SPEED);
-    }
 }
